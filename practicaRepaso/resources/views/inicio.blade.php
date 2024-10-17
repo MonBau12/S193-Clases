715384bce811808542f615f744ea07a6ff5f9458
MonBau12 <montse.bau.sandoval@gmail.com>
2024-10-16 19:53:44 -0600

    PASO 3

---
+++ practicaRepaso/resources/views/inicio.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/js/app.js'])
+
+    <title>Inicio</title>
+    
+    <style>
+        body, html {
+            height: 100%;
+        }
+        .full-height {
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <div class="d-flex flex-column justify-content-center align-items-center text-center full-height">
+        <h1 class="display-1">¡Paola Montserrat Bautista Sandoval!</h1>
+        <a href="{{route ('rutainicio') }}" class="btn btn-warning">repaso1</a>
+    </div>
+</body>
+</html>
