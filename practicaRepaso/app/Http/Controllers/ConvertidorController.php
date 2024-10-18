694fbbc00d27183bb68cbda908901dfd9803c573
MonBau12 <montse.bau.sandoval@gmail.com>
2024-10-17 18:31:28 -0600

    repositorio1

---
+++ practicaRepaso/app/Http/Controllers/ConvertidorController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ConvertidorController extends Controller
+{
+    public function convertir(Request $request)
+    {
+        $valor = $request->input('valor');
+        $unidad = $request->input('unidad');
+
+        switch ($unidad) {
+            case 'MBtoGB':
+                $resultado = $valor / 1024;
+                break;
+            case 'GBtoMB':
+                $resultado = $valor * 1024;
+                break;
+            case 'GBtoTB':
+                $resultado = $valor / 1024;
+                break;
+            case 'TBtoGB':
+                $resultado = $valor * 1024;
+                break;
+            default:
+                return response()->json('Unidad no válida', 400);
+        }
+
+        return response()->json("El resultado de la conversión es: $resultado");
+    }
+}
--- practicaRepaso/routes/web.php
+++ practicaRepaso/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+/*use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+
+use App\Http\Controllers\ConvertidorController;
+
+Route::get('/convertir', [ConvertidorController::class, 'convertir']);
