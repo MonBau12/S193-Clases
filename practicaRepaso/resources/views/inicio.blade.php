966a9a9b8d482bc0e82fc9ed97e9f6f68e2e0ff3
MonBau12 <montse.bau.sandoval@gmail.com>
2024-10-17 18:10:16 -0600

    Revert "Controlador"
    This reverts commit c0700ffe08df483f7e86786e652897236217f443.

--- practicaRepaso/resources/views/inicio.blade.php
+++ practicaRepaso/resources/views/inicio.blade.php
@@ -6,12 +6,20 @@
     @vite(['resources/js/app.js'])
 
     <title>Inicio</title>
+    
+    <style>
+        body, html {
+            height: 100%;
+        }
+        .full-height {
+            height: 100vh;
+        }
+    </style>
 </head>
 <body>
     <div class="d-flex flex-column justify-content-center align-items-center text-center full-height">
         <h1 class="display-1">¡Paola Montserrat Bautista Sandoval!</h1>
-        <h1 class="display-1">Grupo: S193</h1>
-        <a href="{{route ('rutarepaso') }}" class="btn btn-warning">repaso1</a>
+        <a href="{{route ('rutainicio') }}" class="btn btn-warning">repaso1</a>
     </div>
 </body>
 </html>
--- practicaRepaso/routes/web.php
+++ practicaRepaso/routes/web.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-    //return view('welcome');
-//});
-
-route::view('/','inicio')->name('rutainicio');
-route::view('/repaso','repaso1')->name('rutarepaso');
-
-use App\Http\Controllers\ConvertidorController;
-
-Route::get('/convertir', [ConvertidorController::class, 'convertir']);
+Route::get('/', function () {
+    return view('welcome');
+});
