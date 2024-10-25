ee7ec43fe0ea20098fd49d8dd07a3b0d07b0afaa
MonBau12 <montse.bau.sandoval@gmail.com>
2024-10-24 18:02:12 -0600

    PRACTICA 6

--- introLaravel/app/Http/Controllers/ControladorVistas.php
+++ introLaravel/app/Http/Controllers/ControladorVistas.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\validadorCliente;
 
 class ControladorVistas extends Controller
 {
@@ -23,14 +24,35 @@ class ControladorVistas extends Controller
     {
         return view('clientes');
     }
-
-    public function procesarClientes(Request $peticion)
+public function procesarClientes(validadorCliente $peticion)
     {
-       /* return 'Si llego la info del cliente :)';*/
+          //return 'Si llego la info del cliente :)';
 
-       //return $peticion->all();
+        //mostramos todo lo que trae la peticion
+        //return $peticion->all();
+        //return $peticion->path();
+        //return $peticion->url();
+        //return $peticion->ip();
+        //return redirect('/'); //Redirecciona al inicio
+        //return redirect()->route('rutaClientes'); //Redirecciona a la ruta clientes
+        //return back();
 
-       return $peticion->all();
+        //$id= [['usuario'=>1],['usuario'=>2]];
+        //return view('form',compact('id'));
+        
+        $validacion= $peticion->validate([
+            'txtnombre'=> 'required|min:4|max:20',
+            'txtapellidos'=> 'required',
+            'txtcorreo'=> 'required',
+            'txttelefono'=> 'required|numeric',
+        ]);
+       
+        //redireccion con un mensaje flashb
+        $usuario= $peticion->input('txtNombre');
+        
+        session()->flash('exito','Se guardo el usuario: '.$usuario);
+        return to_route('rutaform');
+    
 
 
 
--- introLaravel/resources/views/formulario.blade.php
+++ introLaravel/resources/views/formulario.blade.php
@@ -7,44 +7,68 @@
             {{-- inicia Tarjeta con formulario --}}
 
             <div class="container mt-5 col-md-6">
-                
-                <div class="card font-monospace">
+        {{-- @if(session('exito'))
+        <x-Alert tipo="success"> {{session('exito')}}</x-Alert>
+        @endif --}}
+        {{--@session('exito')
+        <x-Alert style="background-color: greenyellow;" tipo="warning" > {{ $value }}</x-Alert>
+        @endsession--}}
+        
+        @session('exito')
+        {!<script>
+            Swal.fire({
+                title: "Good job!",
+                text: "You clicked the button!",
+                icon: "success"});
+           </script>!}
+        @endsession
 
-                <div class"card-header fs-5 text-center text-primary">
-                    Registro de Clientes
-                </div>
+        <div class="card font-monospace">
 
-                <div class="card-body text-justify">
-
-                    <form action="/enviarClientes" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="nombre" class="form-label"> Nombre: </label>
-                            <input type="text" class="form-control" name="txtnombre">
+            <div class="card-header fs-5 text-center text-primary">
+                Registro de Clientes
             </div>
-            <div class="mb-3">
-                <label for"Apellido" class="form-label"> Apellido: </label>
-                <input type"text" class="form-control" name="txtapellido">
+
+            <div class="card-body text-justify">
+
+                <form class="mb-3" action="/enviarCliente" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label">Nombre: </label>
+                        <input type="text" class="form-control" name="txtnombre" value="{{old('txtnombre')}}" >
+                        <small class="text-danger fst-italic">{{ $errors->first('txtnombre') }}</small>                    
+                    </div>
+                    <div class="mb-3">
+                        <label for="apellidos" class="form-label">Apellidos: </label>
+                        <input type="text" class="form-control" name="txtapellidos" value="{{old('txtapellidos')}}">
+                        <small class="text-danger fst-italic">{{ $errors->first('txtapellidos') }}</small>
+                    </div>
+                    <div class="mb-3">
+                        <label for="correo" class="form-label">Correo: </label>
+                        <input type="text" class="form-control" name="txtcorreo" value="{{old('txtcorreo')}}">
+                        <small class="text-danger fst-italic">{{ $errors->first('txtcorreo') }}</small>
+                    </div>
+                    <div class="mb-3">
+                        <label for="telefono" class="form-label">Telefono: </label>
+                        <input type="text" class="form-control" name="txttelefono" value="{{old('txttelefono')}}">
+                        <small class="text-danger fst-italic">{{ $errors->first('txttelefono') }}</small>
+                    </div>
             </div>
-            <div class="mb-3">
-                <label for"correo" class="form-label"> Correo: </label>
-                <input type"email" class="form-control" name="txtcorreo">      
-            </div>         
-            <div class="mb-3">
-                <label for"telefono" class="form-label"> Telefono: </label>
-                <input type"number" class="form-control" name="txttelefono">      
-            </div> 
-    </div>        
 
-    <div class="card-footer text-muted">
+            <div class="card-footer text-muted">
 
-        <div class="d-grid gap-2 mt-2 mb-1">
-            <button type="submit" class="btn btn-success btn-sm"> Guardar Cliente </button>
+            <div class="d-grid gap-2 mt-2 mb-1">
+                <button type="submit" class="btn btn-success btn-sm"> Guardar Cliente </button>
+            </div>
+
+            </form>
+            </div>
         </div>
-    </form>
-    </div>
-    </div>
     </div> {{-- divcontainer --}}
-    {{-- finaliza Tarjeta con formulario --}}
+    {{-- Finaliza Tarjeta con formulario --}}
 
 @endsection
+
+
+
+
