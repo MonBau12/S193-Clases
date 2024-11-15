8e77a7217bb526ea42f4e02de868e4d55e81f7fa
MonBau12 <montse.bau.sandoval@gmail.com>
2024-11-14 22:08:30 -0600

    commit

---
+++ introLaravel - copia/resources/views/formulario.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.plantilla1')
+
+@section('titulo','Formulario')
+
+@section('contenido')
+
+            {{-- inicia Tarjeta con formulario --}}
+
+            <div class="container mt-5 col-md-6">
+        {{-- @if(session('exito'))
+        <x-Alert tipo="success"> {{session('exito')}}</x-Alert>
+        @endif --}}
+        {{--@session('exito')
+        <x-Alert style="background-color: greenyellow;" tipo="warning" > {{ $value }}</x-Alert>
+        @endsession--}}
+        
+        @session('exito')
+        {!<script>
+            Swal.fire({
+                title: "Good job!",
+                text: "You clicked the button!",
+                icon: "success"});
+           </script>!}
+        @endsession
+
+        <div class="card font-monospace">
+
+            <div class="card-header fs-5 text-center text-primary">
+                 {{__('Registro de Clientes')}}
+            </div>
+
+            <div class="card-body text-justify">
+
+                <form class="mb-3" action="/enviarClientes" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label">{{__('Nombre')}} </label>
+                        <input type="text" class="form-control" name="txtnombre" value="{{old('txtnombre')}}" >
+                        <small class="text-danger fst-italic">{{ $errors->first('txtnombre') }}</small>                    
+                    </div>
+                    <div class="mb-3">
+                        <label for="apellidos" class="form-label">{{__('Apellidos')}}</label>
+                        <input type="text" class="form-control" name="txtapellidos" value="{{old('txtapellidos')}}">
+                        <small class="text-danger fst-italic">{{ $errors->first('txtapellidos') }}</small>
+                    </div>
+                    <div class="mb-3">
+                        <label for="correo" class="form-label">{{__('Correo')}}</label>
+                        <input type="text" class="form-control" name="txtcorreo" value="{{old('txtcorreo')}}">
+                        <small class="text-danger fst-italic">{{ $errors->first('txtcorreo') }}</small>
+                    </div>
+                    <div class="mb-3">
+                        <label for="telefono" class="form-label">{{__('Telefono')}}</label>
+                        <input type="text" class="form-control" name="txttelefono" value="{{old('txttelefono')}}">
+                        <small class="text-danger fst-italic">{{ $errors->first('txttelefono') }}</small>
+                    </div>
+            </div>
+
+            <div class="card-footer text-muted">
+
+            <div class="d-grid gap-2 mt-2 mb-1">
+                <button type="submit" class="btn btn-success btn-sm"> {{__('Guardar Cliente')}} </button>
+            </div>
+
+            </form>
+            </div>
+        </div>
+    </div> {{-- divcontainer --}}
+    {{-- Finaliza Tarjeta con formulario --}}
+
+@endsection
+
+
+
+
---
+++ introLaravel - copia/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\clienteController;
+use App\Http\Controllers\ControladorVistas;
+use Illuminate\Support\Facades\Route;
+
+//RUTA TIPO GET
+/* Route::get('/', function () {
+    return view('welcome');
+}); 
+
+// RUTA TIPO VIEW
+Route::view('/','inicio')->name ('rutainicio');
+
+
+//CREACION DE RURTA DE NUESTRO FORMULARIO DE VISTA 
+//RUTA TIPO VIEW
+Route::view('/form','formulario')->name ('rutaform');
+
+
+// Ruta de clientes
+Route::view('/cliente','clientes')->name('rutaclientes');
+*/
+
+Route::view('/componentes','componentes')->name('rutacomponentes');
+
+
+
+
+/*rutas para controlador    Vistas*/
+
+
+Route::get('/',[ControladorVistas::class, 'home'])->name('rutainicio');
+Route::get('/form',[ControladorVistas::class, 'formulario'])->name('rutaform');
+Route::get('/consultar',[ControladorVistas::class, 'consulta'])->name('rutaclientes');
+
+Route::post('/enviarClientes',[ControladorVistas::class, 'procesarClientes'])->name('rutaEnviar');
+
+
+
+
+/*rutas para cliente controlador */
+
+Route::get('/cliente/create',[clienteController::class, 'create'])->name('rutaform');
